865364c52524a29f9435dbefd003e5e9ab466ca8
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-04-29 01:35:12 +0200

    removed target from repo, overrided equals method for memorycards

--- memory/src/main/java/com/github/shmvanhouten/memory/MemoryCard.java
+++ memory/src/main/java/com/github/shmvanhouten/memory/MemoryCard.java
@@ -5,7 +5,9 @@ import java.io.File;
 public class MemoryCard {
     private static File backOfCard = new File("D:\\mijn documenten\\simonekamer.github.io\\images", "achterkant.jpg");
     private File image;
-    boolean isTurned = false;
+    private boolean isTurned = false;
+
+
 
     MemoryCard(String imageLocation){
         image = new File("D:\\mijn documenten\\simonekamer.github.io\\images", imageLocation);
@@ -13,5 +15,27 @@ public class MemoryCard {
 
     public void turn() {
         isTurned = !isTurned;
+    }
+
+    public boolean isTurned() {
+        return isTurned;
+    }
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MemoryCard that = (MemoryCard) o;
+
+        return image.equals(that.image);
+    }
+
+    @Override
+    public int hashCode() {
+        return image.hashCode();
     }
 }
--- memory/src/test/java/com/github/shmvanhouten/memory/MemoryCardTest.java
+++ memory/src/test/java/com/github/shmvanhouten/memory/MemoryCardTest.java
@@ -10,10 +10,18 @@ public class MemoryCardTest {
     @Test
     public void itShouldTellIfACardIsTurnedOrNot() throws Exception {
         MemoryCard card = new MemoryCard("1.jpg");
-        assertThat(card.isTurned, is(false));
+        assertThat(card.isTurned(), is(false));
         card.turn();
-        assertThat(card.isTurned, is(true));
+        assertThat(card.isTurned(), is(true));
     }
 
+    @Test
+    public void itShouldCompare2Cards() throws Exception {
+        MemoryCard card = new MemoryCard("1.jpg");
+        MemoryCard cardCopy = new MemoryCard("1.jpg");
+        MemoryCard card2 = new MemoryCard("2.jpg");
+        assertThat(card.equals(cardCopy), is(true));
+        assertThat(card.equals(card2), is(false));
+    }
 
 }
