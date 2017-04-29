748b37f0e18e9fc07110f428bb6b6074306ad7e0
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-04-29 18:52:34 +0200

    made shuffleMachine shuffle for players List, also made a shuffle method for Maps, but isn't implemented yet.

--- memory/src/main/java/com/github/shmvanhouten/memory/ShuffleMachine.java
+++ memory/src/main/java/com/github/shmvanhouten/memory/ShuffleMachine.java
@@ -1,9 +1,7 @@
 package com.github.shmvanhouten.memory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 public class ShuffleMachine {
     public Map<Integer,Integer> shuffle(int numberToShuffle, boolean brokenShuffleMachine) {
@@ -20,11 +18,26 @@ public class ShuffleMachine {
 
 
     public List<Player> shuffle(List<Player> playerList, boolean brokenShuffleMachine) {
-        List<Player> shuffledList = new ArrayList<>();
         if(brokenShuffleMachine) {
-            shuffledList = playerList;
+            return playerList;
         }
-        return shuffledList;
+        Collections.shuffle(playerList);
+        return playerList;
+    }
+
+    public Map<Integer, Position> shuffle(Map<Integer, Position> unShuffledMap, boolean brokenShuffleMachine){
+        if(brokenShuffleMachine){
+            return unShuffledMap;
+        }
+        Map<Integer, Position> shuffledMap = new TreeMap<>();
+        List<Integer> integers = new ArrayList<>(unShuffledMap.keySet());
+        Collections.shuffle(integers);
+        int i = 0;
+        for (Integer integer : integers) {
+            shuffledMap.put(i, unShuffledMap.get(integer));
+            i++;
+        }
+        return shuffledMap;
     }
 }
 
